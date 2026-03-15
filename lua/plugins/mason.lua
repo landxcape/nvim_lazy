c974c46038f94494596521b01f9c0d16d08164ee
Purna Kalyan Shakya <38076182+landxcape@users.noreply.github.com>
2026-03-15 15:50:24 +0545

    chore(tooling): ensure stylua via mason and refresh docs

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+
+      if not vim.tbl_contains(opts.ensure_installed, "stylua") then
+        table.insert(opts.ensure_installed, "stylua")
+      end
+    end,
+  },
+}
